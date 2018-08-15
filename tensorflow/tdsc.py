4d475d4685e3fbef1fd5566fc8e474ad8e0514f9
Miao Yin <yinmiaothink@gmail.com>
2018-08-15 22:17:29 +0800

    add

--- tensorflow/tdsc.py
+++ tensorflow/tdsc.py
@@ -29,7 +29,12 @@ class TDSC(object):
 
         self.X_p_hat = tf.fft(tf.complex(self.X_p, tf.zeros([batch_size, n1, n2, n3])))
 
-    def tensor_dl(self, X_p_hat, B, dual_lambda):
+        self.dl_loss = self.compute_tensor_dl_loss(self.X_p_hat, self.B, self.dual_lambda)
+        self.dl_opt = tf.contrib.opt.ScipyOptimizerInterface(self.dl_loss, method='L-BFGS-B')
+
+
+
+    def compute_tensor_dl_loss(self, X_p_hat, B, dual_lambda):
         B_hat = tf.fft(tf.complex(B, tf.zeros([params.r, n2, n3])))
         m = n1
         k = n3
@@ -41,6 +46,21 @@ class TDSC(object):
 
         XB_t = tf.concat([tf.expand_dims(tf.matmul(x_hat, tf.transepos(b_hat)), axis=-1) \
                           for (x_hat, b_hat) in zip(x_hat_list, b_hat_list)], axis=-1)
+
+        bb_hat_list = [tf.matmul(b_hat, tf.transpose(b_hat)) for b_hat in b_hat_list]
+        xb_hat_list = [tf.matmul(x_hat, tf.transpose(b_hat)) for (x_hat, b_hat)
+                       in zip(x_hat_list, b_hat_list)]
+
+        if m > params.r:
+            f = sum([np.trace(tf.matmul(tf.matrix_inverse(bb_hat), tf.matmul(tf.transpose(xb_hat), xb_hat)))
+                    for (bb_hat, xb_hat) in zip(bb_hat_list, xb_hat_list)])
+        else:
+            f = sum([np.trace(tf.matmul(tf.matmul(xb_hat, tf.matrix_inverse(bb_hat)), tf.transpose(xb_hat)))
+                     for (bb_hat, xb_hat) in zip(bb_hat_list, xb_hat_list)])
+
+        return tf.real(f)
+
+
 
         lambda_matrix = tf.diag(dual_lambda)
         tf.trace(tf.matmul())
