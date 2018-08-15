a8dadfe22c4f16794c5f017fb43af01fdc857e26
Xiao-Yang Liu <xl2427@columbia.edu>
2018-08-14 23:02:55 -0400

    Update readme.m

--- msi_denoise_matlab/readme.m
+++ msi_denoise_matlab/readme.m
@@ -8,3 +8,6 @@
 % (1) TenTSTA: using iterative thresholding  for solving the coefficient
 % (2) TenDL  : using the Lagrange dual for the dictionary learning
 %--------------------------------------------------
+
+%%%% for me detailed information, please check our AAAI 2018 
+% F. Jiang, X.-Y. Liu, H. Lu, R. Shen. Efficient multi-dimensional tensor sparse coding using t-linear combinations. AAAI, 2018.
