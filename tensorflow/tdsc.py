79f82d3041392a95032e47908ff418f2a3b723b6
Miao Yin <yinmiaothink@gmail.com>
2018-08-18 22:41:31 +0800

    fix to run

--- tensorflow/tdsc.py
+++ tensorflow/tdsc.py
@@ -28,23 +28,26 @@ class TDSC(object):
         self.B = tf.Variable(np.zeros([params.r, n, k]), dtype=tf.float32)
         self.dual_lambda = tf.Variable(np.random.rand(params.r), dtype=tf.float32)
 
-        # self.X_p_hat = tf.fft(tf.complex(self.X_p, tf.zeros([batch_size, m, n, k])))
-        self.X_p_hat = tf.fft(tf.complex(self.X_p, tf.zeros([m, n, k])))
+        # self.X_p_complex = tf.complex(self.X_p, tf.zeros([m, n, k]))
+        # self.D_complex = tf.complex(self.D, tf.zeros([m, params.r, k]))
+        # self.B_complex = tf.complex(self.B, tf.zeros([params.r, n, k]))
 
+        # tensor sparse coding
         self.B_assign = self.tensor_tsta(self.X_p, self.D, self.B)
 
-        self.dl_loss, self.D_assign = self.tensor_dl(self.X_p_hat, self.B, self.dual_lambda)
+        # tensor dictionry learning
+        self.dl_loss, self.D_assign = self.tensor_dl(self.X_p, self.B, self.dual_lambda)
         self.dl_opt = tf.contrib.opt.ScipyOptimizerInterface(
             self.dl_loss, method='L-BFGS-B', var_to_bounds=(0, np.infty))
 
+        # X reconstruction
         self.X_p_recon = self.tensor_product(self.D, '', self.B, '')
 
-    def tensor_dl(self, X_p_hat, B, dual_lambda):
-        B_hat = tf.fft(tf.complex(B, tf.zeros([params.r, self.n, self.k])))
-        m = self.m
-        k = self.k
-        x_hat_list = [tf.squeeze(x) for x in tf.split(X_p_hat, k)]
-        b_hat_list = [tf.squeeze(b) for b in tf.split(B_hat, k)]
+    def tensor_dl(self, X_p, B, dual_lambda):
+        X_p_hat = tf.fft(tf.complex(X_p, tf.zeros(tf.shape(X_p))))
+        B_hat = tf.fft(tf.complex(B, tf.zeros(tf.shape(B))))
+        x_hat_list = [tf.squeeze(x) for x in tf.split(X_p_hat, self.k, axis=-1)]
+        b_hat_list = [tf.squeeze(b) for b in tf.split(B_hat, self.k, axis=-1)]
 
         # BB_t = tf.concat([tf.expand_dims(tf.matmul(b_hat, tf.transepos(b_hat)), axis=-1)
         #                   for b_hat in b_hat_list], axis=-1)
@@ -56,14 +59,15 @@ class TDSC(object):
         xb_hat_list = [tf.matmul(x_hat, tf.transpose(b_hat)) for (x_hat, b_hat)
                        in zip(x_hat_list, b_hat_list)]
 
-        lambda_mat = tf.diag(dual_lambda)
+        lambda_diag = tf.diag(dual_lambda)
+        lambda_mat = tf.complex(lambda_diag, tf.zeros(tf.shape(lambda_diag)))
 
-        if m > params.r:
-            f = sum([np.trace(tf.matmul(tf.matrix_inverse(bb_hat + lambda_mat),
+        if self.m > params.r:
+            f = sum([tf.trace(tf.matmul(tf.matrix_inverse(bb_hat + lambda_mat),
                                         tf.matmul(tf.transpose(xb_hat), xb_hat)))
                     for (bb_hat, xb_hat) in zip(bb_hat_list, xb_hat_list)])
         else:
-            f = sum([np.trace(tf.matmul(tf.matmul(xb_hat, tf.matrix_inverse(bb_hat + lambda_mat)),
+            f = sum([tf.trace(tf.matmul(tf.matmul(xb_hat, tf.matrix_inverse(bb_hat + lambda_mat)),
                                         tf.transpose(xb_hat)))
                      for (bb_hat, xb_hat) in zip(bb_hat_list, xb_hat_list)])
 
@@ -71,7 +75,7 @@ class TDSC(object):
                                                                  tf.transpose(xb_hat))), axis=-1)
                            for (bb_hat, xb_hat) in zip(bb_hat_list, xb_hat_list)], axis=-1)
 
-        D_ = tf.ifft(D_hat)
+        D_ = tf.real(tf.ifft(D_hat))
         D_assign = tf.assign(self.D, D_)
 
         return tf.real(f), D_assign
@@ -83,7 +87,7 @@ class TDSC(object):
         # DD_cmat = blk_circ_mat(DD)
         # l0 = tf.norm(DD_cmat, 2)
         l0 = tf.norm(DD, 2)
-        DX = self.tensor_product(D, 't', X, '')
+        DX = self.tensor_product(D, 't', X_p, '')
 
         C1 = B0
         t1 = 1
@@ -103,10 +107,10 @@ class TDSC(object):
         return B_assign
 
     def tensor_product(self, P, ch1, Q, ch2):
-        P_hat = tf.fft(P)
-        Q_hat = tf.fft(Q)
-        p_hat_list = [tf.squeeze(p) for p in tf.split(P_hat, self.k)]
-        q_hat_list = [tf.squeeze(q) for q in tf.split(Q_hat, self.k)]
+        P_hat = tf.fft(tf.complex(P, tf.zeros(tf.shape(P))))
+        Q_hat = tf.fft(tf.complex(Q, tf.zeros(tf.shape(Q))))
+        p_hat_list = [tf.squeeze(p) for p in tf.split(P_hat, self.k, axis=-1)]
+        q_hat_list = [tf.squeeze(q) for q in tf.split(Q_hat, self.k, axis=-1)]
 
         # x_hat_list = [tf.squeeze(t) for t in tf.split(X_p_hat, k)]
 
@@ -114,33 +118,19 @@ class TDSC(object):
         #                   for (x_hat, b_hat) in zip(x_hat_list, b_hat_list)], axis=-1)
 
         if ch1 == 't' and ch2 == 't':
-            S_hat = tf.concat([tf.expand_dims(tf.matmul(tf.transpose(p_hat), tf.transpose(q_hat)))
+            S_hat = tf.concat([tf.expand_dims(tf.matmul(tf.transpose(p_hat), tf.transpose(q_hat)), axis=-1)
                                for (p_hat, q_hat) in zip(p_hat_list, q_hat_list)], axis=-1)
         elif ch1 == 't':
-            S_hat = tf.concat([tf.expand_dims(tf.matmul(tf.transpose(p_hat), q_hat))
+            S_hat = tf.concat([tf.expand_dims(tf.matmul(tf.transpose(p_hat), q_hat), axis=-1)
                                for (p_hat, q_hat) in zip(p_hat_list, q_hat_list)], axis=-1)
         elif ch2 == 't':
-            S_hat = tf.concat([tf.expand_dims(tf.matmul(p_hat, tf.transpose(q_hat)))
+            S_hat = tf.concat([tf.expand_dims(tf.matmul(p_hat, tf.transpose(q_hat)), axis=-1)
                                for (p_hat, q_hat) in zip(p_hat_list, q_hat_list)], axis=-1)
         else:
-            S_hat = tf.concat([tf.expand_dims(tf.matmul(p_hat, q_hat))
+            S_hat = tf.concat([tf.expand_dims(tf.matmul(p_hat, q_hat), axis=-1)
                                for (p_hat, q_hat) in zip(p_hat_list, q_hat_list)], axis=-1)
 
-        return tf.ifft(S_hat)
-
-    def train(self, sess):
-        init = tf.global_variables_initializer()
-        sess.run(init)
-
-        for i in range(params.sc_max_iter):
-            # compute tensor coefficients B
-            sess.run(self.B_assign)
-
-            # compute tensor dictionary D
-            self.dl_opt.minimize(sess)
-            sess.run(self.D_assign)
-
-
+        return tf.real(tf.ifft(S_hat))
 
     @staticmethod
     def init_D(patch_size, r):
@@ -163,8 +153,18 @@ if __name__ == '__main__':
 
     tdsc = TDSC(m, n, k, 1)
 
+    init = tf.global_variables_initializer()
     with tf.Session() as sess:
-        tdsc.train(sess)
+        sess.run(init)
+
+        for i in range(params.sc_max_iter):
+            print('Iteration: {} / {}'.format(i, params.sc_max_iter),)
+            # compute tensor coefficients B
+            sess.run(tdsc.B_assign, feed_dict={tdsc.X_p:X_p})
+
+            # compute tensor dictionary D
+            tdsc.dl_opt.minimize(sess, feed_dict={tdsc.X_p:X_p})
+            sess.run(tdsc.D_assign, feed_dict={tdsc.X_p:X_p})
 
         X_p_recon = sess.run(tdsc.X_p_recon)
         X_recon = block_3d_tensor(X_p_recon, np.shape(X))
