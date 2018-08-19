db5abb1fe5b33e316e8b10d13b3e39e62ed36ec9
ymthink <yinmiaothink@gmail.com>
2018-08-19 15:49:15 +0800

    stable

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Time: 2018/8/12 14:40
 
 import time
+import os
 import scipy.io as sio
 import numpy as np
 from init import *
@@ -13,19 +14,34 @@ from tensor_tsta import *
 from hyper_params import HyperParams as params
 from matplotlib import pyplot as plt
 
+
+def save_img(img, file_name):
+    fig = plt.figure(figsize=(5, 15))
+    ax = fig.add_subplot(111)
+    plt.axis('off')
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.set_aspect('equal')
+    plt.imshow(img, cmap='Greys_r')
+    plt.savefig(file_name, bbox_inches='tight')
+    plt.close(fig)
+
 def tdsc():
     X = sio.loadmat('samples/baloons_101_101_31.mat')['Omsi']
     size_X = np.shape(X)
-    plt.imshow(X[:,:,1])
-    plt.show()
 
     X_p = tensor_block_3d(X)
     size_X_p = np.shape(X_p)
     X_p_hat = np.fft.fft(X_p, axis=-1)
 
-    D = init_3d_tensor(params.patch_size, params.r)
+    #D = init_3d_tensor(params.patch_size, params.r)
+    D = sio.loadmat('./samples/D0.mat')['D0']
     B = np.zeros([params.r, size_X_p[1], size_X_p[2]])
 
+    if not os.path.exists('./out/'):
+        os.mkdir('./out/')
+
+    save_img(X[:,:,2], './out/origin.png')
 
     for i in range(params.sc_max_iter):
         time_s = time.time()
@@ -38,8 +54,7 @@ def tdsc():
 
         X_p_ = tensor_product(D, '', B, '')
         X_ = block_3d_tensor(X_p_, size_X)
-        plt.imshow(X_[:,:,1])
-        plt.show()
+        save_img(X_[:,:,2], './out/{}.png'.format(str(i).zfill(3)))
 
         time_e = time.time()
         print('time:', time_e - time_s, 's')
