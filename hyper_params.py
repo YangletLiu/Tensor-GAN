d0dbb7aadc5835eedc71ce76e172c3cf3c3e8dfb
Miao Yin <yinmiaothink@gmail.com>
2018-08-28 23:20:49 +0800

    rename file

--- hyper_params.py
+++ hyper_params.py
@@ -1,10 +1,10 @@
 
 class HyperParams():
     # patch_size = 5
-    patch_size = 3
+    patch_size = 3 # for rgb image
     step = 1
     # r = 30
-    r = 24
+    r = 24 # for rgb image
     eta = 1.01
     tsta_max_iter = 50
     beta = 0.8
