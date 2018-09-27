0086b04cdb558557248e6eda05b0a4603e535e0b
YIN MIAO <yinmiaothink@gmail.com>
2018-09-27 12:06:12 +0800

    commit

--- tensorflow/aae.py
+++ tensorflow/aae.py
@@ -9,6 +9,7 @@ import sys
 import scipy.io as sio
 import os
 import keras
+from skimage import transform
 from matplotlib import pyplot as plt
 
 
@@ -70,7 +71,9 @@ class AAE(object):
 
         return keras.Model(z, d)
 
-    def train(self, batch_size, train_data, iter_num):
+    def train(self, batch_size, train_data, iter_num, folder):
+        if not os.path.exists(folder):
+            os.mkdir(folder)
 
         real = np.ones([batch_size, 1])
         fake = np.zeros([batch_size, 1])
@@ -93,8 +96,8 @@ class AAE(object):
             if i % 100 == 0:
                 print('epoch {}: D loss: {}, G loss: {}, AutoEncoder loss: {}'.format(i, d_loss, g_loss, ae_loss))
                 samples = train_data[np.random.randint(0, train_data.shape[0], 8)]
-                gen_samples = aae.autoencoder.predict(samples)
-                aae.save_samples(samples, gen_samples, i, 'imgs')
+                gen_samples = self.autoencoder.predict(samples)
+                self.save_samples(samples, gen_samples, i, folder)
 
         return d_loss, g_loss, ae_loss
 
@@ -103,14 +106,14 @@ class AAE(object):
 
         fig = plt.figure(figsize=(samples.shape[0], 2))
         for i in range(samples.shape[0]):
-            ax = fig.add_subplot(2, samples.shape[0], 2*i+1)
+            ax = fig.add_subplot(2, samples.shape[0], i+1)
             plt.axis('off')
             ax.set_xticklabels([])
             ax.set_yticklabels([])
             ax.set_aspect('equal')
             plt.imshow(np.squeeze(samples[i]), cmap='Greys_r')
 
-            ax = fig.add_subplot(2, samples.shape[0], 2*i+2)
+            ax = fig.add_subplot(2, samples.shape[0], samples.shape[0]+i+1)
             plt.axis('off')
             ax.set_xticklabels([])
             ax.set_yticklabels([])
@@ -122,13 +125,61 @@ class AAE(object):
         fig.savefig("./{}/iter_{}.png".format(folder, epoch), bbox_inches='tight')
         plt.close()
 
+class SR(object):
+
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+        x = keras.Input(shape=self.input_shape)
+        h = keras.layers.Flatten()(x)
+        h = keras.layers.Dense(256, activation='relu')(h)
+        h = keras.layers.Dense(512, activation='relu')(h)
+        h = keras.layers.Dense(np.prod(self.output_shape), activation='tanh')(h)
+        y = keras.layers.Reshape(self.output_shape)(h)
+        self.srer = keras.Model(x, y)
+        optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.5)
+        self.srer.compile(optimizer=optimizer, loss='mse')
+
+    def train(self, batch_size, x_data, y_data, iter_num):
+        for i in range(iter_num):
+            indices = np.random.randint(0, x_data.shape[0], batch_size)
+            xs = x_data[indices]
+            ys = y_data[indices]
+            loss = self.srer.train_on_batch(xs, ys)
+            if i % 100 == 0:
+                print('SR loss={}'.format(loss))
+
 
 if __name__ == '__main__':
-    aae = AAE([32, 32, 3], [32, 32, 3], 256)
     batch_size = 32
-    epochs = 10000
-    (train_data, _), (_, _) = keras.datasets.cifar10.load_data()
-    train_data = (train_data.astype(np.float32) - 127.5) / 127.5
-    aae.train(batch_size, train_data, epochs)
+    epochs = 2000
+    # (x_train, y_train), (_, _) = keras.datasets.mnist.load_data()
+    (x_train, _), (_, _) = keras.datasets.cifar10.load_data()
+    x_train = (x_train.astype(np.float32) - 127.5) / 127.5
+    # train_data = x_train[np.where(y_train == 1)[0]]
+    # train_data = np.expand_dims(x_train, 3)
+    train_data = x_train
+    train_data0 = []
+    for i in range(x_train.shape[0]):
+        train_data0.append(transform.resize(x_train[i], (16, 16)))
+    train_data0 = np.array(train_data0)
+    # train_data0 = np.expand_dims(train_data0, 3)
+    aae0 = AAE([16, 16, 3], [16, 16, 3], 128)
+    aae0.train(batch_size, train_data0, epochs, 'img0')
+    sr = SR([16, 16, 3], [32, 32, 3])
+    sr.train(batch_size, train_data0, train_data, epochs)
+    aae1 = AAE([32, 32, 3], [32, 32, 3], 256)
+    aae1.train(batch_size, train_data, epochs, 'img1')
+
+    indices = np.random.randint(0, train_data0.shape[0], 8)
+    zs = np.random.normal(size=[batch_size, 128])
+    samples = train_data0[indices]
+    gs = aae0.decoder.predict(zs)
+    gs = sr.srer.predict(gs)
+    gs = aae1.autoencoder.predict(gs)
+    AAE.save_samples(gs, gs, 0, 'img')
+
+
 
 
